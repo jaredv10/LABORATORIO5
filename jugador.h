32f8187e944892c1a5590060da8136a18d78369d
jaredv10 <160081426+jaredv10@users.noreply.github.com>
2024-05-22 16:22:59 -0500

    jugador.h

--- jugador.h
+++ jugador.h
@@ -1,19 +1,30 @@
 #ifndef JUGADOR_H
 #define JUGADOR_H
 
+#include <QGraphicsScene>
+#include <QGraphicsPixmapItem>
+#include <QPixmap>
 #include <QGraphicsRectItem>
 #include <QObject>
 #include <QKeyEvent>
+#include <QPainter>
 
 class Jugador : public QObject, public QGraphicsRectItem {
     Q_OBJECT
 
 public:
     Jugador();
+    QRectF boundingRect() const override;
+    void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget) override;
+    void advance(int phase) override;
     void keyPressEvent(QKeyEvent *event) override;
 
 signals:
     void objectDestroyed(const QString &tipoObjeto);
+
+private:
+    QPixmap imagen;
+    void destruirObjetos();
 };
 
 #endif // JUGADOR_H
