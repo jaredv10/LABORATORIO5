d923743cb4e41ab79440552bf2bf431214adc84c
jaredv10 <160081426+jaredv10@users.noreply.github.com>
2024-05-22 16:23:45 -0500

    main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
+#include <QApplication>
+#include <QTime>
 #include "mainwindow.h"
 
-#include <QApplication>
-
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
+    qsrand(QTime::currentTime().msec());
+
     MainWindow w;
     w.show();
     return a.exec();
 }
+
